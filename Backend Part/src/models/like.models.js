24e4e799237bd866386322eda148e5e73b6c7118
First code demo <akhtarifarhin@gmail.com>
2026-09-09 11:37:07 +0530

    Handle like notification duplication

--- Backend Part/src/models/like.models.js
+++ Backend Part/src/models/like.models.js
@@ -19,6 +19,10 @@ const likeSchema = new Schema({
          },
 },
 {timestamps: true}
-)
+);
+likeSchema.index(
+    { video: 1, likedBy: 1 },
+    { unique: true }
+);
 
 export const Like = mongoose.model("Like", likeSchema)
--- Backend Part/src/models/notification.models.js
+++ Backend Part/src/models/notification.models.js
@@ -35,4 +35,5 @@ const notificationSchema = new Schema({
 
 }, {timestamps: true})
 
+
 export const Notification = mongoose.model("Notification", notificationSchema)
